b7f5eb0d52c2048886837a111d804a561247b0ab
Denish Fuletra <fuletradenish@gmail.com>
2023-02-07 21:17:25 +0530

    added mogno

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true)
 
 async function ConnectDatabase() {
-    let result = await mongoose.connect("mongodb+srv://Zostel:<password>@cluster0.mt1xxf3.mongodb.net/?retryWrites=true&w=majority/Zostel")
+    let result = await mongoose.connect("mongodb+srv://Zostel:zostel12345@cluster0.mt1xxf3.mongodb.net/?retryWrites=true&w=majority/Zostel")
     return result;
 }
 
